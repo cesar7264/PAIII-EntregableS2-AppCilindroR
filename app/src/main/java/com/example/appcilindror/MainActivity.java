c58636ead72b4caea95634b41b6f90ef43d3ec36
cesar7264 <cteranh14@unc.edu.pe>
2025-02-23 21:50:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appcilindror/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.appcilindror;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    //Declaramos los objetos de ingreso de datos
+    EditText txtRadio, txtAltura;
+    TextView lbVolumen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        //Inicilizar los objetos de tipo visuales
+        txtRadio = findViewById(R.id.txtRadio);
+        txtAltura = findViewById(R.id.txtAltura);
+        lbVolumen= findViewById(R.id.lbVolumen);
+        lbVolumen.setText("");
+
+    }
+
+    public void CalcularVolumen (View boton){
+        if(Validar()) //Si validar retorna true
+            return;
+        //leer los datos en los ediText
+        Double radio = Double.valueOf(txtRadio.getText().toString());
+        Double altura = Double.valueOf(txtAltura.getText().toString());
+        Double volumen = Math.PI*Math.pow(radio,2)*altura;
+        lbVolumen.setText(volumen+"");
+    }
+
+    private boolean Validar() {
+        if(txtRadio.getText().toString().trim().isEmpty()){
+            txtRadio.setError("Campo obligatorio para el radio de la base");
+            txtRadio.requestFocus();
+            return  true;
+        }
+
+        if(txtAltura.getText().toString().trim().isEmpty()){
+            txtAltura.setError("Campo obligatorio para el volumen del cilindro");
+            txtAltura.requestFocus();
+            return  true;
+        }
+
+        return false;
+    }
+}
